e50474bd14991cc3fc709c827b746737b3c21255
viswanath <akkak@gmail.com>
2025-01-20 22:23:34 +0530

    added backend code for large image uploads

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,7 +16,8 @@ dotenv.config();
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb" }));
 app.use(cookieParser());
 app.use(
   cors({
